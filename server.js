d5f9dc1e38eb8424896cf733dfe14dae37ea0405
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-16 22:44:14 -0600

    db refactor

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ const app        = express();
 const morgan     = require('morgan');
 
 // PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
+const db = require('./lib/db.js');
 db.connect();
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
@@ -34,12 +32,10 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/users", usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
 
