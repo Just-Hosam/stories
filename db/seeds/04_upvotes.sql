027969f998f26f05a5cf66411db220fb9b3b07ad
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-03-16 23:41:31 -0600

    seed correction

--- db/seeds/04_upvotes.sql
+++ db/seeds/04_upvotes.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (user_id, contribution_id, isLiked) VALUES
+INSERT INTO upvotes (user_id, contribution_id, isLiked) VALUES
 (1, 1, true),
 (1, 2, true),
 (1, 3, true),
@@ -10,4 +10,4 @@ INSERT INTO users (user_id, contribution_id, isLiked) VALUES
 (2, 3, true),
 (2, 4, true),
 (2, 5, true),
-(2, 6, true),
+(2, 6, true);
